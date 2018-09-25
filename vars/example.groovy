2cb60dce43648b38a7275b86ac46df698f6b30ce
shreya1396 <42531964+shreya1396@users.noreply.github.com>
2018-09-25 10:38:41 +0530

    Update example.groovy

--- vars/example.groovy
+++ vars/example.groovy
@@ -13,23 +13,10 @@ def call(body) {
                 stage ('Clone') {
                     checkout scm
                 }
-                stage ('Build') {
-                    sh "echo 'building ${config.projectName} ...'"
+                stage ('Test') {
+                    sh "echo 'Testing ${config.projectName} ...'"
                 }
-                stage ('Tests') {
-                    parallel 'static': {
-                        sh "echo 'shell scripts to run static tests...'"
-                    },
-                    'unit': {
-                        sh "echo 'shell scripts to run unit tests...'"
-                    },
-                    'integration': {
-                        sh "echo 'shell scripts to run integration tests...'"
-                    }
-                }
-                stage ('Deploy') {
-                    sh "echo 'deploying to server ${config.serverDomain}...'"
-                }
+               
             } catch (err) {
                 currentBuild.result = 'FAILED'
                 throw err
